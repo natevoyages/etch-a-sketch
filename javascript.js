6aefa5b0ca44873558e0a3901b0582f6f8a2fd67
NateVoyages <allanromo20@gmail.com>
2022-09-22 10:25:23 -0700

    Clean code + Remove borders to look authentic

--- javascript.js
+++ javascript.js
@@ -3,30 +3,7 @@ let num = 16;
 const divs = [];
 const children = [];
 
-for(let j = 0; j < num * num; j++){
-  children.push(document.createElement("div"));
-  children[j].style.backgroundColor = "white";
-  children[j].style.border = "1px solid black";
-  children[j].style.width = "10px";
-  children[j].style.height = "auto";
-  children[j].style.flex = "1";
-}
-
-for(let i = 0; i < num; i++){
-  divs.push(document.createElement("div"));
-  divs[i].classList.add(i);
-  divs[i].style.display = "flex";
-  divs[i].style.backgroundColor = "white";
-  divs[i].style.width = "700px";
-  divs[i].style.height = "auto";
-  divs[i].style.flex = "1 1 auto";
-
-
-  for(let j = 0; j < num; j++){
-    children[j + (i * num)].classList.add(j + (i * num));            //changed to numbers instead of dashes
-    divs.forEach(div => div.appendChild(children[j+(i * num)]));     //  to make sketch function simpler
-  } 
-}
+setGrid();
 divs.forEach(div => content.appendChild(div));
 
 
@@ -36,24 +13,15 @@ children.forEach(child => child.addEventListener('mouseover', sketch));
 const button = document.querySelector("button");
 button.addEventListener('click', changeGrid);
 
-function changeGrid(){
-  let prevNum = num;
-  num = prompt("How many number of squares per side for the new grid? (Limit 100)");
-  if (num <= 100 && num > 0){
-    divs.forEach(div => div.remove());
-    for(let i = 0; i < prevNum; i++){
-      divs.pop();
-    }
+function setGrid(){
 
-
-for(let j = 0; j < num * num; j++){
-  children.push(document.createElement("div"));
-  children[j].style.backgroundColor = "white";
-  children[j].style.border = "1px solid black";
-  children[j].style.width = "10px";
-  children[j].style.height = "auto";
-  children[j].style.flex = "1";
-}
+  for(let i = 0; i < num * num; i++){
+    children.push(document.createElement("div"));
+    children[i].style.backgroundColor = "white";
+    children[i].style.width = "10px";
+    children[i].style.height = "auto";
+    children[i].style.flex = "1";
+  }
 
 for(let i = 0; i < num; i++){
   divs.push(document.createElement("div"));
@@ -63,6 +31,7 @@ for(let i = 0; i < num; i++){
   divs[i].style.width = "700px";
   divs[i].style.height = "auto";
   divs[i].style.flex = "1 1 auto";
+  
 
 
   for(let j = 0; j < num; j++){
@@ -74,14 +43,23 @@ divs.forEach(div => content.appendChild(div));
 
 
 children.forEach(child => child.addEventListener('mouseover', sketch));
-  }
 
 }
 
+function changeGrid(){
+  let prevNum = num;
+  num = prompt("How many number of squares per side for the new grid? (Limit 100)");
+  if (num <= 100 && num > 0){
+    divs.forEach(div => div.remove());
+    for(let i = 0; i < prevNum; i++){
+      divs.pop();
+    }
+    setGrid();
+  }
+}
 
 
-
-function sketch(e){
+function sketch(){
   children[this.classList.value].style.backgroundColor = "black";
 
 }
